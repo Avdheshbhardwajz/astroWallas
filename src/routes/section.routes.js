8aa076f8a5aec1ee5c4efea501c4c70d94e371a7
Avdhesh Bhardwaj <140992329+Avdheshbhardwajz@users.noreply.github.com>
2024-11-24 21:09:55 +0530

    Update section.routes.js

--- src/routes/section.routes.js
+++ src/routes/section.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { editSection, fetchSection } from "../controllers/section.controller.js";
+import { editSection, fetchSection , fetchData } from "../controllers/section.controller.js";
 
 const sectionRouter = express.Router();
 
 sectionRouter.get("/fetch-section", fetchSection);
+sectionRouter.get("/fetch-data", fetchData);
 sectionRouter.put("/edit-section", editSection);
 
 export default sectionRouter;
