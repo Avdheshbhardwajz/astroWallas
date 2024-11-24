78db12e59320cce5d400366817b311763e66133d
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-24 22:01:14 +0530

    installed cors and used.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import sectionRouter from "./routes/section.routes.js";
 import connection from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/sections", sectionRouter);
 
